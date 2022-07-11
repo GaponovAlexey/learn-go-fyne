12dc5719ff9c205cebb95f8b931a8a288522ff24
kekelberi <alexei.gaponov@gmail.com>
2022-07-11 20:40:50 +0300

    save

--- regist/main.go
+++ regist/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -13,9 +16,12 @@ func main() {
 	a := app.New()
 	a.Settings().SetTheme(theme.DarkTheme())
 	w := a.NewWindow("Hello from GO")
+	ic, _ := fyne.LoadResourceFromPath("f.ico")
+	w.SetIcon(ic)
 	w.Resize(fyne.NewSize(500, 400))
 
 	label := widget.NewLabel("Регистрация")
+	label.TextStyle = fyne.TextStyle{Bold: true}
 	label.Move(fyne.NewPos(200, 2))
 	//entry
 	name := widget.NewEntry()
@@ -23,7 +29,7 @@ func main() {
 	name.Move(fyne.NewPos(100, 60))
 	name.SetPlaceHolder("Name")
 
-	pass := widget.NewEntry()
+	pass := widget.NewPasswordEntry()
 	pass.Resize(fyne.NewSize(300, 40))
 	pass.Move(fyne.NewPos(100, 120))
 	pass.SetPlaceHolder("Пароль")
@@ -35,11 +41,17 @@ func main() {
 
 	dopInfo := widget.NewEntry()
 	dopInfo.Resize(fyne.NewSize(300, 40))
+	dopInfo.Wrapping = fyne.TextWrapBreak
 	dopInfo.Move(fyne.NewPos(100, 240))
 	dopInfo.SetPlaceHolder("Дополнительная информация")
 	//btn
-	btn := widget.NewButton("Зарегистрироваться", func() {})
-	btn.Resize(fyne.NewSize(200, 50))
+	btn := widget.NewButton("Зарегистрироваться", func() {
+		file, _ := os.Create("regist/user.txt")
+		defer file.Close()
+		data := fmt.Sprintf("name:%v\npass:%v\nemail:%v\ndopInfo:%v\n", name.Text, pass.Text, email.Text, dopInfo.Text)
+		file.WriteString(data)
+	})
+	btn.Resize(fyne.NewSize(200, 40))
 	btn.Move(fyne.NewPos(150, 300))
 	//cont
 	cont := container.NewWithoutLayout(label, name, pass, email, dopInfo, btn)
