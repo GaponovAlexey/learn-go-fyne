8c4e8ca613b5fdce7aaa10747f46dc4024567c65
kekelberi <alexei.gaponov@gmail.com>
2022-07-11 15:01:32 +0300

    save

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -17,24 +15,15 @@ func main() {
 	w := a.NewWindow("Hello from GO")
 	w.Resize(fyne.NewSize(900, 600))
 
-	// img2
-	circl2 := canvas.NewCircle(color.Black)
-	//рамка
-	circl2.StrokeColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
-	circl2.StrokeWidth = 5 // рамка цвет
-	// квадрат
-	rectangle := canvas.NewRectangle(color.NRGBA{R: 77, G: 55, B: 33, A: 255})
-	// линия
-	line:= canvas.NewLine(color.NRGBA64{R: 122, G: 33, B: 56, A: 255})
-	line.StrokeColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
-	line.StrokeWidth = 5 // рамка цвет
-	//icon
-	icon:= widget.NewIcon(theme.FileIcon())
+	oval1 := canvas.NewCircle(color.NRGBA{R: 155, G: 55, B: 33, A: 255})
+	oval2 := canvas.NewCircle(color.NRGBA{R: 155, G: 55, B: 33, A: 255})
+	oval3 := canvas.NewCircle(color.NRGBA{R: 155, G: 55, B: 33, A: 255})
+	oval4 := canvas.NewCircle(color.NRGBA{R: 155, G: 55, B: 33, A: 255})
 
-	cont1 := container.NewGridWithColumns(2,rectangle,icon)
-	cont2 := container.NewGridWithColumns(2,line, circl2)
+	row1 := container.NewGridWithRows(2, oval1, oval2)
+	row2 := container.NewGridWithRows(2, oval3, oval4)
 
-	content := container.NewGridWithRows(2, cont1, cont2)
+	content := container.NewGridWithColumns(2, row1, row2)
 	w.SetContent(
 		content,
 	)
